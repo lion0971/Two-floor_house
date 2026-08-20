a3f77175c1be4f8c3bc48198592fecc0e1d1a45f
lion0971 <liongirlroam@gmail.com>
2026-08-20 23:55:18 +0800

    path_changed

--- scene-config.js
+++ scene-config.js
@@ -1,17 +1,11 @@
 // scene-config.js
 const isGitHub = window.location.hostname.includes('github.io');
-const basePath = isGitHub ? '/3D_Module_Web/' : './';
+const basePath = isGitHub ? '/Two-floor_house/' : './';
 
 export const CONFIG = {
     MODELS: {
-        PLANT: `${basePath}models/Plant_Turtle_LOD.glb`,
         BUILDING: `${basePath}models/20260805.glb`,
         HDRI: `${basePath}hdri/studio-0623.hdr`
-    },
-    ROOM_DATA: {
-        "in_door1": "會議室 101",
-        "in_door2": "會議室 102",
-        "in_door3": "茶水間"
     },
    CAMERA: {
         fov: 60,
